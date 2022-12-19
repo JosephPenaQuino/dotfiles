03c96b12bc38dfb4420bf3dc8fb1fef1299d267d
Joseph P <joseph.pena@utec.edu.pe>
2022-12-19 01:38:09 -0300

    feat: nvim: add new plugins: gitsigns lualine

--- .config/nvim/lua/joseph/packer.lua
+++ .config/nvim/lua/joseph/packer.lua
@@ -11,7 +11,7 @@ return require('packer').startup(function(use)
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
+		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
 	use({
 		'rose-pine/neovim',
@@ -20,40 +20,55 @@ return require('packer').startup(function(use)
 			vim.cmd('colorscheme rose-pine')
 		end
 	})
-	use(
+	use(-- Highlight, edit, and navigate code
 		'nvim-treesitter/nvim-treesitter',
-		{run = ':TSUpdate'}
+		{ run = ':TSUpdate' }
 	)
+	use {
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		after = 'nvim-treesitter'
+	}
 	--- This is used for my own plugins
 	use('nvim-treesitter/playground')
 	use('theprimeagen/harpoon')
 	use('mbbill/undotree')
+
+	use('lewis6991/gitsigns.nvim') -- Git signs
 	use('tpope/vim-fugitive')
 	use('tpope/vim-commentary')
+
+	use {
+		'nvim-lualine/lualine.nvim', -- Fancier status line
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+
+	use 'lukas-reineke/indent-blankline.nvim' -- Indent guides
+	use 'tpope/vim-sleuth' -- Automatically set indent settings
+
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		requires = {
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+			{ 'neovim/nvim-lspconfig' },
+			{ 'williamboman/mason.nvim' },
+			{ 'williamboman/mason-lspconfig.nvim' },
 
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
+			{ 'hrsh7th/nvim-cmp' }, -- Autocompletion
+			{ 'hrsh7th/cmp-buffer' },
+			{ 'hrsh7th/cmp-path' },
+			{ 'saadparwaiz1/cmp_luasnip' },
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'hrsh7th/cmp-nvim-lua' },
 
 			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
+			{ 'L3MON4D3/LuaSnip' },
+			{ 'rafamadriz/friendly-snippets' },
 		}
 	}
-    use("folke/zen-mode.nvim")
-    use('github/copilot.vim')
-    use('christoomey/vim-system-copy')
-    use('vim-scripts/argtextobj.vim')
-    use('RRethy/vim-illuminate')
+	use("folke/zen-mode.nvim")
+	use('github/copilot.vim')
+	use('christoomey/vim-system-copy')
+	use('vim-scripts/argtextobj.vim')
+	use('RRethy/vim-illuminate')
 end)
