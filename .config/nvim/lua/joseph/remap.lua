7707c5903352cad8389754b1133fd1f80e4325e7
Joseph P <joseph.pena@utec.edu.pe>
2022-12-19 02:37:59 -0300

    feat: nvim: add leader>k

--- .config/nvim/lua/joseph/remap.lua
+++ .config/nvim/lua/joseph/remap.lua
@@ -40,3 +40,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- clear highlights after search
+vim.keymap.set("n", "<leader>k", "<cmd>nohl<CR>")
--- .config/nvim/lua/joseph/set.lua
+++ .config/nvim/lua/joseph/set.lua
@@ -20,7 +20,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true -- Highlight the token while typing a search command
 
 vim.opt.termguicolors = true
