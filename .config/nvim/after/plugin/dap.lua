8c1b2612cdf0a44111a4765a71036079e6ac3af7
Joseph P <joseph.pena@utec.edu.pe>
2023-05-29 19:40:23 -0300

    feat: nvim: add config for dap, fidget, neotest, overseer, and todo_comments

---
+++ .config/nvim/after/plugin/dap.lua
@@ -0,0 +1,72 @@
+local dap = require('dap')
+
+
+-- dap.configurations.python = {
+-- 	{
+-- 		type = 'python',
+-- 		request = 'launch',
+-- 		name = "Launch file",
+-- 		program = "${file}",
+-- 		pythonPath = function()
+-- 			return '/usr/bin/python'
+-- 		end,
+-- 	},
+-- }
+
+
+
+vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+vim.keymap.set("n", "<leader>dc", dap.continue)
+vim.keymap.set("n", "<leader>dd", dap.disconnect)
+vim.keymap.set("n", "<leader>de", dap.step_out)
+vim.keymap.set("n", "<leader>di", dap.step_into)
+vim.keymap.set("n", "<leader>do", dap.step_over)
+vim.keymap.set("n", "<leader>dr", dap.repl.open)
+vim.keymap.set("n", "<leader>ds", dap.stop)
+
+
+-- set some custom styles for the debugger
+local dap_breakpoint = {
+	error = {
+		text = "🟥",
+		texthl = "LspDiagnosticsSignError",
+		linehl = "",
+		numhl = "",
+	},
+	rejected = {
+		text = "",
+		texthl = "LspDiagnosticsSignHint",
+		linehl = "",
+		numhl = "",
+	},
+	stopped = {
+		text = "⭐️",
+		texthl = "LspDiagnosticsSignInformation",
+		linehl = "DiagnosticUnderlineInfo",
+		numhl = "LspDiagnosticsSignInformation",
+	},
+}
+
+vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+
+
+require("nvim-dap-virtual-text").setup {
+	commented = true,
+}
+local dapui = require "dapui"
+dapui.setup {} -- use default
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+local dap_python = require('dap-python')
+dap_python.setup("/usr/bin/python3.11")
+dap_python.test_runner = 'pytest'
