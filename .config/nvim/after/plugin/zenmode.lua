4a54110982eb549bf1e649d6c69a23129c3533c9
Joseph P <joseph.pena@hc.fm.usp.br>
2024-05-07 11:29:10 -0300

    🐛 (nvim): remove duplicated theme call

--- .config/nvim/after/plugin/zenmode.lua
+++ .config/nvim/after/plugin/zenmode.lua
@@ -11,5 +11,5 @@ require("zen-mode").setup {
 vim.keymap.set("n", "<leader>zz", function()
     require("zen-mode").toggle()
     vim.wo.wrap = false
-    ColorMyPencils()
+    -- ColorMyPencils()
 end)
