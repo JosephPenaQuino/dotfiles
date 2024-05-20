9bb5883f4b065ca7019db3aa0342f2f4cc87b00f
Joseph P <joseph.pena@hc.fm.usp.br>
2024-05-20 09:28:27 -0300

    🔧 (nvim): remove grammarly configurations because their API is no more available

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -26,10 +26,10 @@ require('mason-lspconfig').setup({
     lua_ls = function()
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
-      require('lspconfig').grammarly.setup{
-           -- on_attach = on_attach,
-           init_options = { clientId = os.getenv("GRAMMARLY_ID") }
-      }
+      -- require('lspconfig').grammarly.setup{
+      --      -- on_attach = on_attach,
+      --      init_options = { clientId = os.getenv("GRAMMARLY_ID") }
+      -- }
     end,
   }
 })
