7c0546cd5a9075778aa6386b2fa6c14d5eda4c0a
Joseph P. <joseph.pena.q@gmail.com>
2024-05-23 15:41:53 -0300

    🔀 (nvim): add t as prefix for telescope keymaps

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,18 +1,18 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>F', builtin.find_files, {})
-vim.keymap.set('n', '<leader>G', function()
+vim.keymap.set('n', '<leader>tF', builtin.find_files, {})
+vim.keymap.set('n', '<leader>tG', function()
 	builtin.git_files({recurse_submodules = true})
 end, {})
-vim.keymap.set('n', '<leader>g', function()
+vim.keymap.set('n', '<leader>tg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<leader>B', builtin.buffers, {})
+vim.keymap.set('n', '<leader>tB', builtin.buffers, {})
 
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>H', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>tH', builtin.help_tags, {})
 
 
-vim.keymap.set('n', '<leader>D', ":Telescope diagnostics<CR>")
+vim.keymap.set('n', '<leader>tD', ":Telescope diagnostics<CR>")
 
 local telescope = require('telescope')
 
