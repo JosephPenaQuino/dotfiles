84ffeec18859ed674698eb14487b33fb2bff187f
Joseph P. <joseph.pena.q@gmail.com>
2024-09-25 09:52:40 -0300

    🔧 add keymaps to move between cells and add configuration to use # %%

--- .config/nvim/after/plugin/vim_slime.lua
+++ .config/nvim/after/plugin/vim_slime.lua
@@ -1,7 +1,8 @@
 vim.g.slime_target = "tmux"
-vim.g.slime_cell_delimiter = "#%%"
+vim.g.slime_cell_delimiter = "# %%"
 vim.g.slime_default_config = { socket_name = "default", target_pane = "0" }
 vim.g.slime_dont_ask_default = 1
+vim.g.slime_bracketed_paste = 1
 
 vim.api.nvim_set_keymap('x', '<leader>s', '<Plug>SlimeRegionSend', {})
 vim.api.nvim_set_keymap('n', '<leader>s', '<Plug>SlimeMotionSend', {})
