3beab8513963daf8fefc49e438da5a284ef086fd
Joseph P. <joseph.pena.q@gmail.com>
2024-10-16 15:19:18 -0300

    🔧(nvim): move git dependencies from basic to git lua/plugin config file

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,4 +1,7 @@
 return {
+    { 'rhysd/git-messenger.vim' },
+    { 'lewis6991/gitsigns.nvim' },
+    { 'tpope/vim-fugitive' },
     {
 	'JosephPenaQuino/committer.nvim',
 	config = function()
@@ -7,6 +10,7 @@ return {
 	dependencies = {
 	    'nvim-telescope/telescope.nvim',
 	    'olacin/telescope-gitmoji.nvim',
+	    'olacin/telescope-cc.nvim',
 	    'nvim-lua/plenary.nvim',
 	},
     }
